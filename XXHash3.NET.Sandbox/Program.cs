eccd2a0cd104879584cf3fb01125a7eb432e9b9b
Crauzer <filip.quitko@gmail.com>
2021-05-14 10:59:59 +0200

    uh

---
+++ XXHash3.NET.Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace XXHash3.NET.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong x = XXHash3.Hash64(Encoding.UTF8.GetBytes("ABCDEF"));
+            string g = $"{x:x16}";
+        }
+    }
+}
