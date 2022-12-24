7ce114e357747e713bb2dec14d4f915af2195eef
Crauzer <filip.quitko@gmail.com>
2022-12-24 22:35:24 +0100

    Update Program.cs

--- XXHash3.NET.Sandbox/Program.cs
+++ XXHash3.NET.Sandbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -6,17 +7,24 @@ namespace XXHash3NET.Sandbox
 {
     class Program
     {
+        private static readonly (string file, string checksum) TESTSTREAM_XXH = ("teststream.xxh", "0x5841c4fe86cbde7e");
+
         static void Main(string[] args)
         {
-            XXH3FileDigest("teststream.xxh");
+            XXH3FileDigest(TESTSTREAM_XXH.file, TESTSTREAM_XXH.checksum);
         }
 
-        static void XXH3FileDigest(string file)
+        static void XXH3FileDigest(string file, string originalChecksum)
         {
             using FileStream stream = File.OpenRead(file);
 
             ulong digest = XXHash3.Hash64(stream);
-            
+
+            string checksum = string.Format("0x{0:x}", digest);
+            if (checksum != originalChecksum)
+            {
+
+            }
         }
     }
 }
